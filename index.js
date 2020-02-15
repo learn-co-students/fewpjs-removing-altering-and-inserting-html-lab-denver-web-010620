ee1ee5e4000445743d8f264d5b47bed0a537d0f0
etgreer91 <etgreer91@gmail.com>
2020-02-15 11:50:51 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let element = document.querySelector("main#main");
+document.body.removeChild(element);
+
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory")
+console.log(newHeader)
+
+newHeader.innerHTML = "Evan is the champion";
